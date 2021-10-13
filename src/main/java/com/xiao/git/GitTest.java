73cc383330c26fc55a0a516c25162b6798a1cf8d
肖尹黎 <74492884+xiao997@users.noreply.github.com>
2021-10-13 18:08:53 +0800

    Update GitTest.java

--- src/main/java/com/xiao/git/GitTest.java
+++ src/main/java/com/xiao/git/GitTest.java
@@ -11,5 +11,6 @@ public class GitTest {
         System.out.println("hello Git2");
         System.out.println("hello Git3");
         System.out.println("hello hot-fix");
+        System.out.println("pull the code");
     }
 }
