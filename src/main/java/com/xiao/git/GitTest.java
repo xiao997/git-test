3d4b81c46ee51695a742bf8628f14224491ea5f2
xiaoYL <xiaoYL@123.com>
2021-10-13 16:12:56 +0800

    first commit

---
+++ src/main/java/com/xiao/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.xiao.git;
+
+/**
+ * @Author: 肖尹黎
+ * @CreateData: 2021-10-13 16:04
+ * @Description:
+ **/
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello Git");
+    }
+}
